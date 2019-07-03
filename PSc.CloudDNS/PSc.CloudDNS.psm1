3e8b70a67383fd689c67371f4c2256fdb434b67e
Josh Wright <josh.wright100@gmail.com>
2019-07-03 23:12:03 +0100

    Created Module psm1 and psd1 files

---
+++ PSc.CloudDNS/PSc.CloudDNS.psm1
@@ -0,0 +1,24 @@
+﻿<#
+    ===========================================================================
+
+	-------------------------------------------------------------------------
+	 Module Name: PSc.CloudDNS
+	===========================================================================
+#>
+
+#Get public and private function definition files.
+$Public = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -Recurse -ErrorAction SilentlyContinue )
+$Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -Recurse -ErrorAction SilentlyContinue )
+
+#Dot source the files
+Foreach ($Import in @($Public + $Private)) {
+    Try {
+        . $Import.FullName
+    }
+    Catch {
+        Write-Error -Message "Failed to import function $($import.fullname): $_"
+    }
+}
+
+# Export Public functions
+Export-ModuleMember -Function $Public.Basename
